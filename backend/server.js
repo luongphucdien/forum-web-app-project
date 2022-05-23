47dba90d3fae97a6a694a0f1a37abf86f303d6ca
luongphucdien <work.luongphucdien@gmail.com>
2022-05-24 00:58:11 +0700

    Dien | Update | Update files

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,21 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 
-app.use(cors());
+var corsOptions = {
+    origin: 'http://localhost:8080'
+};
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.listen(8080, () => console.log('Connection success. API is running on port 8080'));
+const PORT = 8080;
+app.listen(PORT, () => console.log('Connection success. API is running on port ' + PORT));
+
+
+const db = require('./app/models');
+const User = db.User;
+db.sequelize.sync();
+
+
+require('./app/routes/auth.routes')(app);
+require('./app/routes/user.routes')(app);
